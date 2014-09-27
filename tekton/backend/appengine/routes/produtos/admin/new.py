49cb26717fd9d70d6b46f4e7e66df0be7bf54466
allan2k1 <allan_christian10@hotmail.com>
2014-09-27 14:33:17 -0300

    teste

--- tekton/backend/appengine/routes/produtos/admin/new.py
+++ tekton/backend/appengine/routes/produtos/admin/new.py
@@ -2,18 +2,24 @@
 from __future__ import absolute_import, unicode_literals
 from config.template_middleware import TemplateResponse
 from gaebusiness.business import CommandExecutionException
+from gaeforms.ndb.form import ModelForm
 from tekton import router
 from gaecookie.decorator import no_csrf
 from produto_app import facade
 from routes.produtos import admin
+from tekton.backend.apps.produto_app.model import ProdForm
 
 
 @no_csrf
 def index():
     return TemplateResponse({'save_path': router.to_path(save)},'produtos/admin/form.html')
 
+def save(_logged_user,**produto_properties):
+    prod_form = ProdForm(**produto_properties)
 
-def save(_handler, produto_id=None, **produto_properties):
+
+
+
     cmd = facade.save_produto_cmd(**produto_properties)
     try:
         cmd()
@@ -23,4 +29,3 @@ def save(_handler, produto_id=None, **produto_properties):
 
         return TemplateResponse(context, 'produtos/admin/form.html')
     _handler.redirect(router.to_path(admin))
-
